c1a4b3b1edefeb3c080527466782bd796e4d3b19
SpectreTerminal <31022245+SpectreTerminal@users.noreply.github.com>
2022-01-12 20:52:54 -0500

    whoops

---
+++ meal360/src/dietMgmtBackend.js
@@ -0,0 +1,9 @@
+import { sendToDB } from "./extCommMod.js";
+
+/**
+ * Temporary stub method to get and send dietary preferences from the database.
+ * 
+ * @param {object} userParams User's new preferences
+ * @returns User's current preferences
+ */
+export const dietaryManagement = userParams => sendToDB(null);
--- meal360/src/mealHistoryBackend.js
+++
@@ -1,9 +0,0 @@
-import { sendToDB } from "./extCommMod.js";
-
-/**
- * Temporary stub method to get meal history from the database.
- * 
- * @param {object} userInfo User's information
- * @returns Meal history of the user
- */
-export const getMealHistory = userInfo => sendToDB(null);
