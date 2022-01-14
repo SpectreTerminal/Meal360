63b4c96671c3e8274180c55ce3b93484fdc6cf3c
Nishanth Raveendran <raveendn@mcmaster.ca>
2022-01-13 21:15:46 -0500

    updated restrictions and login pages

--- meal360-frontend/src/pages/DietaryRestrictions/index.js
+++ meal360-frontend/src/pages/DietaryRestrictions/index.js
@@ -1,8 +1,44 @@
+import { Button, Navbar, Nav, Container, Card, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 import AppNavbar from "../../components/navbar";
 
+var calorieValue
+
+function CalorieRange(event) {
+    console.log(event.target.value)
+    calorieValue = event.target.value
+} 
+
 export default function DietaryRestrictionsPage() {
+  var calorieValue = 0
+
   return (<>
     <AppNavbar active={'dietaryRestrictions'}/>
-    <p>dietary Restrictions</p>
+
+  <Container>
+    <h1 className="text-secondary">Dietary Restrictions</h1>
+    <Card className='w-50 p-3'>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Diet</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option value="1">No Diet</option>
+            <option value="2">Vegetarian</option>
+            <option value="3">Vegan</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Calories</Form.Label>
+          <Form.Control aria-label="Amount (to the nearest dollar)" />
+        </Form.Group>
+        <Link to='/'>
+          <Button variant='primary' type='submit'>Apply</Button>
+        </Link>
+      </Form>
+    </Card>
+  </Container>
+
   </>)
 }
--- meal360-frontend/src/pages/Login/index.js
+++ meal360-frontend/src/pages/Login/index.js
@@ -1,10 +1,40 @@
-import { Button } from "react-bootstrap";
+import { Button, Navbar, Nav, Container, Card, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  return (
-    <Link to='/'>
-      <Button variant='primary'>Login</Button>
-    </Link>
+  return (<>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#">Meal360</Navbar.Brand>
+      </Container>
+    </Navbar>
+    
+    <Container>
+    <h1 className="text-secondary">Login / Create Account</h1>
+    <Card className='w-50 p-3'>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Link to='/'>
+          <Button variant='primary' type='submit'>Login</Button>
+        </Link>
+        <Link to='/'>
+          <Button className='m-1' variant='primary'>Create Account</Button>
+        </Link>
+      </Form>
+    </Card>
+    </Container></>
+
+
   )
 }
