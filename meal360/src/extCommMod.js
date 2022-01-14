fa65bd2ee89328f1c0db19a17d8b8551c00b8400
SpectreTerminal <31022245+SpectreTerminal@users.noreply.github.com>
2022-01-13 21:52:30 -0500

    Finalized changes

--- meal360/src/extCommMod.js
+++ meal360/src/extCommMod.js
@@ -4,10 +4,8 @@ import fetch from 'node-fetch';
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc, deleteDoc, updateDoc} from "firebase/firestore";
 
-// TODO: Note: Filepath changes depending on current directory... might need a way to improve this.
-// Is there a way to make this privately accessible?
+// initialized information
 const _key = FileReader.readFileSync('./apiKey.txt', 'utf8');
-
 initializeApp({
     apiKey: "AIzaSyD70HLFk3PA2uNigsYFEMhKHZPfXO9Nxvk",
     authDomain: "meal360.firebaseapp.com",
@@ -18,7 +16,7 @@ initializeApp({
     measurementId: "G-2FN2DK8L6G"
 });
 const db = getFirestore();
-// TODO: When user creates account, an ID is generated for them. This ID is used as the identifier for their dietary preferences when POSTing, PUTting, GETting or DELETing
+
 
 /**
  * Send information to the requested API with parameters. Returns a Promise object. 
@@ -26,7 +24,7 @@ const db = getFirestore();
  * Example: **let response = await sendToAPI(...)**
  * 
  * @param {string} url - URL of API
- * @param {object} params - parameters to send
+ * @param {object} params - Parameters to send
  * @returns {Promise<unknown>} Response data from API
  */
 export const sendToAPI = (url, params) => {
@@ -36,81 +34,59 @@ export const sendToAPI = (url, params) => {
 }
 
 /**
- * Send information to database. TODO stub method.
+ * GET information from database.
  * 
- * @param {string} schema
- * @param {string} id
+ * @param {string} schema - Name of schema to get information from
+ * @param {string} id - User's unique identifier to retrieve data from schema
  * @returns {DocumentSnapshot<DocumentData>} Query response
  */
 export const getFromDB = async (schema, id) => {
-    // TODO db, schema, ID
-    // example: doc(db, "dietPref", "dietPref2");
     const dietPrefRef = doc(db, schema, id);
 
     // GET DB info
     const querySnapshot = await getDoc(dietPrefRef);
-    console.log(querySnapshot.id);
-    console.log(querySnapshot.data());
+    // console.log(querySnapshot.id);
+    // console.log(querySnapshot.data());
     return querySnapshot;
 }
 
 /**
- * TODO what about registration?
- * @param {string} schema 
- * @param {string} id 
- * @param {object} params 
+ * POST information to database.
+ * 
+ * @param {string} schema - Name of schema to POST information to
+ * @param {string} id - User's unique identifier
+ * @param {object} params - Attributes to POST to the database
  */
 export const postDB = async (schema, id, params) => {
-    // TODO db, schema, ID
     const dietPrefRef = doc(db, schema, id);
     
     // set DB (POST)
-    // await setDoc(dietPrefRef, {
-    //     dietType: "vegetarian",
-    //     exclude: ["olives"],
-    //     targetCalories: 2500,
-    //     user: "sumOthrGuy"
-    // });
     await setDoc(dietPrefRef, params);
 }
 
 /**
+ * Update/PUT information to database.
  * 
- * @param {string} schema 
- * @param {string} id 
- * @param {object} params 
+ * @param {string} schema - Name of schema to PUT information to
+ * @param {string} id - User's unique identifier
+ * @param {object} params - Attributes to update for a user record (can be a subset of all attributes)
  */
 export const updateDB = async (schema, id, params) => {
-    // TODO db, schema, ID
     const dietPrefRef = doc(db, schema, id);
-
-    // PUT DB
-    // await updateDoc(dietPrefRef, {
-    //     dietType: "meat only lol",
-    //     targetCalories: 10000
-    // });
+    
+    // PUT
     await updateDoc(dietPrefRef, params);
 }
 
+/**
+ * DELETE a record from the database.
+ * 
+ * @param {string} schema - Name of schema to DELETE information from
+ * @param {string} id - User's unique identifier
+ */
 export const deleteRecord = async (schema, id) => {
-    // TODO db, schema, ID
     const dietPrefRef = doc(db, schema, id);
 
     // DELETE DB
-    // await deleteDoc(doc(db, "dietPref", "dietPref"));
     await deleteDoc(dietPrefRef);
 }
-
-
-// const dietPrefRef = doc(db, "dietPref", "dietPref2");
-// await setDoc(dietPrefRef, {
-//         dietType: "vegetarian",
-//         exclude: ["olives"],
-//         targetCalories: 2500,
-//         user: "sumOthrGuy"
-//     });
-
-// await updateDoc(dietPrefRef, {
-//         dietType: "meat only lol",
-//         targetCalories: 10000
-//     });
