009bcd005c5e2b09b94009809050c8104fe268e7
SpectreTerminal <31022245+SpectreTerminal@users.noreply.github.com>
2022-01-13 21:55:46 -0500

    Finalized changes

--- meal360/src/dietMgmtBackend.js
+++ meal360/src/dietMgmtBackend.js
@@ -1,11 +1,13 @@
 import { getFromDB, postDB } from "./extCommMod.js";
 
 /**
- * Send the updated dietary preferences to the database
+ * Send the updated dietary preferences to the database. 
+ * Ensure that all calls made to this function use the **await** keyword.
+ * Example: **let response = await sendNewPrefs(...)**
  * 
  * @param {object} userParams User's new preferences
  * @param {string} id User's identifier
- * @returns User's current preferences
+ * @returns {DocumentSnapshot<DocumentData>} User's current preferences
  */
 export const sendNewPrefs = (userParams, id) => {
     postDB("dietPref", id, userParams);
@@ -13,7 +15,9 @@ export const sendNewPrefs = (userParams, id) => {
 }
 
 /**
- * Get the user's dietary preferences
+ * Get the user's dietary preferences.
+ * Ensure that all calls made to this function use the **await** keyword.
+ * Example: **let response = await getPrefs(...)**
  * 
  * @param {string} id User's identifier
  * @returns {DocumentSnapshot<DocumentData>} Query response
@@ -21,12 +25,14 @@ export const sendNewPrefs = (userParams, id) => {
 export const getPrefs = id => getFromDB("dietPref", id);
 
 
-// // TEST Stub
+// TEST Stub
 // const testData = {
 //     dietType: "vegan",
 //     exclude: ["olives"],
-//     targetCalories: 2500,
+//     targetCalories: 500,
 //     user: "test"
 // }
-// const response = sendNewPrefs(testData, "dietPref");
-// console.log(response);
+// await sendNewPrefs(testData, "dietPref");
+// const r2 = await getPrefs("dietPref");
+// console.log(r2.id);
+// console.log(r2.data());
