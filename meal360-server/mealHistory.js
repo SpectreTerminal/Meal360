f3b8b89ffc6d1179976910205ace9ef6960bc9c8
Vanshaj Verma <vermav2@mcmaster.ca>
2022-01-23 00:37:04 -0500

    Fixed/updated Meal history module components

--- meal360-server/mealHistory.js
+++ meal360-server/mealHistory.js
@@ -1,48 +1,56 @@
-import { postDB, getFromDB, updateDB, deleteRecord } from "./extCommMod";
+import { postDB, getFromDB, updateDB, deleteRecord } from "./extCommMod.js";
 
 /**
  * Stores user's meal history in the database.
  * 
- * @param {string} id User's unique identifier
- * @param {object} meal Meal to be stored
- * @returns {DocumentSnapshot<DocumentData>} Query response
- * @throws {Error} If the user's meal history is not found
+ * @param {string} id Unique identifier
+ * @param {object} params JSON object literal of attributes of the meal
  */
-export const storeMeal = async (id, meal) => {
-    postDB("mealHistory", id, meal);
-    return getFromDB("mealHistory", id);
+export const storeMeal = async (id, params) => {
+    postDB("meal-history", id, params);
 }
 
 /**
  * Retrieves user's meal history from the database.
  * 
- * @param {string} id User's unique identifier
- * @returns {DocumentSnapshot<DocumentData>} Query response
+ * @param {string[]} attributes List of attributes to search on
+ * @param {string[]} operators List of operators (ex. ==, <=, <, etc.)
+ * @param {any[]} values List of values of attributes
+ * @returns {DocumentSnapshot<DocumentData>} All records that match the specified clauses
  * @throws {Error} If the user's meal history is not found
  */
-export const getMealHistory = async (id) => {
-    return getFromDB("mealHistory", id);
+export const getMealHistory = async (attributes, operators, values) => {
+    const response = await getFromDB("meal-history", attributes, operators, values);
+    const docs = response.docs;
+    if(docs.length === 0) {
+        throw new Error("Meal history not found");
+    }
+    const records = [];
+    for (let index = 0; index < docs.length; index++) {
+        records[index] = docs[index].data(); 
+    }
+    return records;
 }
 
 /**
  * Delete a user's meal history from the database.
  * 
- * @param {string} id User's unique identifier
- * @returns {DocumentSnapshot<DocumentData>} Query response
- * @throws {Error} If the user's meal history is not found
+ * @param {string[]} attributes List of attributes to search on
+ * @param {string[]} operators List of operators (ex. ==, <=, <, etc.)
+ * @param {any[]} values List of values of attributes
 */
-export const deleteMealHistory = async (id) => {
-    return deleteRecord("mealHistory", id);
+export const deleteMealHistory = async (attributes, operators, values) => {
+    deleteRecord("meal-history", attributes, operators, values);
 }
 
 /**
  * Updates user's meal history in the database.
  * 
- * @param {string} id User's unique identifier
- * @param {object} meal Meal to be updated
- * @returns {DocumentSnapshot<DocumentData>} Query response
- * @throws {Error} If the user's meal history is not found
+ * @param {string[]} attributes List of attributes to search on
+ * @param {string[]} operators List of operators (ex. ==, <=, <, etc.)
+ * @param {any[]} values List of values of attributes
+ * @param {object} params Attributes to update for the user's meal
  */
-export const updateMealHistory = async (id, meal) => {
-    return updateDB("mealHistory", id, meal);
+export const updateMealHistory = async (attributes, operators, values, params) => {
+    updateDB("meal-history", attributes, operators, values, params);
 }
