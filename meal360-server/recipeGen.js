3fc5990f5934a702cc3d6e9ac8592a25dd838b57
Vanshaj Verma <vermav2@mcmaster.ca>
2022-01-23 14:25:08 -0500

    Added function to get recipe info

--- meal360-server/recipeGen.js
+++ meal360-server/recipeGen.js
@@ -1,9 +1,9 @@
 import { sendToAPI } from "./extCommMod.js";
 
 /**
- * generates recipe data from the Spoonacular API call and
+ * Searches for recipes from the Spoonacular API call and
  * based on the user's meal plan preferences and returns the
- * response data. 
+ * list of recipes and other response data. 
  * 
  * @param {String} diet - Diet type for the meal plan
  * @param {String} excludeIngredients - Excluded ingredients for the meal plan
@@ -17,5 +17,22 @@ export async function generateRecipe(diet, excludeIngredients, intolerances) {
         intolerances
     };
     const response = await sendToAPI("https://api.spoonacular.com/recipes/complexSearch", params);
+    return response;
+}
+
+
+/**
+ * Gets the recipe information from the Spoonacular API call and
+ * based on the recipe id and returns the recipe information.
+ * 
+ * @param {String} id - Recipe id
+ * @returns {Object} Response data from the API call
+ */
+ export async function getRecipeInfo(id) {
+    const includeNutrition = true;
+    const params = {
+        includeNutrition
+    };
+    const response = await sendToAPI(`https://api.spoonacular.com/recipes/${id}/information`, params);
     return response;
 }
